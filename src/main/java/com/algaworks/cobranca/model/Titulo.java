4b5c5071445db71a5d6bdb9cb870849fadd95e15
gilsonsilvati <gilsonsilvati@gmail.com>
2016-01-19 10:33:36 -0200

    Teste da primeira versão

--- src/main/java/com/algaworks/cobranca/model/Titulo.java
+++ src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -71,6 +71,10 @@ public class Titulo {
 		this.status = status;
 	}
 	
+	public boolean isPendente() {
+		return StatusTitulo.PENDENTE.equals(this.status);
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(codigo);
